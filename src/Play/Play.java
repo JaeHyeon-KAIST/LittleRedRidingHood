976c8d11004b594589be06270abdd71e19a5d8a0
JunginPark <pji0310@kaist.ac.kr>
2024-10-25 03:48:03 +0900

    Add Scene 3

--- src/Play/Play.java
+++ src/Play/Play.java
@@ -12,18 +12,21 @@ import Play.PlayObject.House.House;
 import Play.Scene.Scene;
 import Play.Scene.Scene1;
 import Play.Scene.Scene2;
+import Play.Scene.Scene3;
 import Play.Scene.TestScene;
 
 public class Play {
     protected String mPlayName = null;
     public List<Scene> mSceneList = new ArrayList<>();
     public Human littleRedRidingHood = null;
+    public Human grandma = null;
     public PigType1 pig1 = null;
     public PigType1 pig2 = null;
     public PigType2 pig3 = null;
     public Wolf wolf = null;
 
     public static House pigHouse = null;
+    public static House grandmaHouse = null;
 
     public void displayPlayTitle() {
         display("<<" + this.mPlayName + ">>");
@@ -49,6 +52,7 @@ public class Play {
 
     private void initializeActors() {
         littleRedRidingHood = new Human("Little Red Riding Hood");
+        grandma = new Human("Grandma");
         pig1 = new PigType1("Adam", "flute");
         pig2 = new PigType1("Brian", "fiddle");
         pig3 = new PigType2("Charles", "piano");
@@ -57,11 +61,13 @@ public class Play {
 
     private void initializeProps() {
         pigHouse = new House("pig's house");
+        grandmaHouse = new House("grandma's house");
     }
 
     private void initializeScenes() {
         mSceneList.add(new Scene1());
         mSceneList.add(new Scene2());
+        mSceneList.add(new Scene3());
         mSceneList.add(new TestScene());
     }
 }
