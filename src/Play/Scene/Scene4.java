e2580552c98c7957f120ff586e002c4192839c52
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-10-26 00:00:35 +0900

    Update Scene 4

--- src/Play/Scene/Scene4.java
+++ src/Play/Scene/Scene4.java
@@ -1,8 +1,5 @@
 package Play.Scene;
 
-import Play.Play;
-import Play.PlayObject.Animal.Wolf;
-
 public class Scene4 extends Scene {
     public Scene4() {
         super("4. " + mplay.wolf.getName() + " chases " +
@@ -14,8 +11,8 @@ public class Scene4 extends Scene {
     public void play() {
         mplay.wolf.disguiseRevealed();
         mplay.wolf.say("You Ain't Seen the half of a ???");
-//        mplay.littleRedRidingHood.surprised();
-//        mplay.littleRedRidingHood.runFrom(mplay.wolf);
-//        mplay.wolf.chase(mplay.littleRedRidingHood);
+        mplay.littleRedRidingHood.surprised();
+        mplay.littleRedRidingHood.runAwayFrom(mplay.wolf);
+        mplay.wolf.chase(mplay.littleRedRidingHood);
     }
 }
