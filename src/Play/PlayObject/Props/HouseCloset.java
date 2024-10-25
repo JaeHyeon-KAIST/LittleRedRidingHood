909f88d2f8ad8a70f478f0032b76f16b41d2dc61
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-10-25 23:56:58 +0900

    Add House Props

---
+++ src/Play/PlayObject/Props/HouseCloset.java
@@ -0,0 +1,9 @@
+package Play.PlayObject.Props;
+
+import Play.PlayObject.PlayObject;
+
+public class HouseCloset extends PlayObject {
+    public HouseCloset(String name) {
+        super(name);
+    }
+}
---
+++ src/Play/PlayObject/Props/HouseDoor.java
@@ -0,0 +1,9 @@
+package Play.PlayObject.Props;
+
+import Play.PlayObject.PlayObject;
+
+public class HouseDoor extends PlayObject {
+    public HouseDoor(String name) {
+        super(name);
+    }
+}
