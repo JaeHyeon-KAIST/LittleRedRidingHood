b0ec1039db6a680bf1b2a15e32d002587eefa0f0
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-10-25 18:42:53 +0900

    Update Scene 1

--- src/Play/Scene/Scene1.java
+++ src/Play/Scene/Scene1.java
@@ -1,5 +1,7 @@
 package Play.Scene;
 
+import java.util.Arrays;
+
 public class Scene1 extends Scene {
     public Scene1() {
         super("1. " + mplay.littleRedRidingHood.getName() +
@@ -35,26 +37,23 @@ public class Scene1 extends Scene {
                 "Take the long road around the forest while the " +
                 "wolf is in the wood."
         );
-//        mplay.pig1.laugh();
+        mplay.pig1.laugh();
         mplay.pig1.sayWith(mplay.pig2, "that old wolf is just a sussy. " +
             "All he does is huff and puff. We'll go with you and protect " +
             "you. Come along. We'll call his bluff."
         );
-//        mplay.pig1.dance();
-//        mplay.pig2.dance();
-//        mplay.pig1.sing("Who's afraid of the Big Bad Wolf? Big Bad Wolf,
-//        Big Bad Wolf? Who's afraid of the Big Bad Wolf?");
-//        mplay.pig2.sing("Who's afraid of the Big Bad Wolf? Big Bad Wolf,
-//        Big Bad Wolf? Who's afraid of the Big Bad Wolf?");
+        mplay.pig1.danceWith(mplay.pig2);
+        mplay.pig1.singTogether(mplay.pig2, "Who's afraid of the Big Bad " +
+            "Wolf? Big Bad Wolf, Big Bad Wolf? Who's afraid of the Big Bad " +
+            "Wolf?"
+        );
         mplay.pig1.playMusicalInstrument();
         mplay.pig2.playMusicalInstrument();
-//        mplay.pig1.walkTo(mplay.forest);
-//        mplay.pig2.walkTo(mplay.forest);
-//        mplay.littleRedRidingHood.walkTo(mplay.forest);
-//        mplay.pig3.clickTongue();
+        mplay.littleRedRidingHood.walkToWith(
+            Arrays.asList(mplay.pig1, mplay.pig2),
+            mplay.forest
+        );
+        mplay.pig3.clickTongue();
         mplay.pig3.repairHouse();
-//        mplay.pig1.walkTo(mplay.forest);
-//        mplay.pig2.walkTo(mplay.forest);
-//        mplay.littleRedRidingHood.walkTo(mplay.forest);
     }
 }
