f5b98ae48b78951a79cae5f91e75fa7f478456d7
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2024-10-25 23:57:29 +0900

    Update House

--- src/Play/PlayObject/Place/House.java
+++ src/Play/PlayObject/Place/House.java
@@ -1,9 +1,25 @@
 package Play.PlayObject.Place;
 
 import Play.PlayObject.PlayObject;
+import Play.PlayObject.Props.HouseCloset;
+import Play.PlayObject.Props.HouseDoor;
 
 public class House extends PlayObject {
+    protected HouseDoor mDoor = null;
+
+    public HouseDoor getDoor() {
+        return mDoor;
+    }
+
+    protected HouseCloset mCloset = null;
+
+    public HouseCloset getCloset() {
+        return mCloset;
+    }
+
     public House(String name) {
         super(name);
+        mDoor = new HouseDoor(name + "'s door");
+        mCloset = new HouseCloset(name + "'s closet");
     }
 }
